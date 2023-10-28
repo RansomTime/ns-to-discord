1e5bf03b067a6b3fc728d09c9e370d5365727e2c
Alex Bryson <alex@ransomti.me>
2023-10-28 13:13:36 +0100

    timestamp now tracks how long you've been in range

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,29 @@ use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
 use std::{thread, time};
 
 const CONVERSION_FACTOR: f32 = 18.016;
+#[derive(PartialEq, Eq, Copy, Clone)]
+enum BgRange {
+    High,
+    Normal,
+    Low,
+}
 
 #[derive(Deserialize, Debug, Clone)]
 struct NsResults
 {
-    sgv: i32,
-    delta: i32,
+    sgv: f32,
+    delta: f32,
     direction: String,
     date: i64,
 }
 
 impl NsResults {
     fn sgv_as_mmol(&self) -> f32 {
-        self.sgv as f32 / CONVERSION_FACTOR
+        self.sgv / CONVERSION_FACTOR
     }
 
     fn delta_as_mmol(&self) -> f32 {
-        self.delta as f32 / CONVERSION_FACTOR
+        self.delta  / CONVERSION_FACTOR
     }
 
     fn direction_to_char(&self) -> char {
@@ -40,13 +46,32 @@ impl NsResults {
         }
     }
 
+    fn get_range(&self) -> BgRange {
+        let mmol = self.sgv_as_mmol();
+        if mmol > 10.0 {
+            BgRange::High
+        } else if mmol > 3.8 {
+            BgRange::Normal
+        } else {
+            BgRange::Low
+        }
+    }
+
     fn to_status_str(&self) -> String {
-        format!("{:.1} mmol/L",
-            &self.sgv_as_mmol())
+        let range_state = match self.get_range() {
+            BgRange::High => " (high)",
+            BgRange::Low => " (low)",
+            BgRange::Normal => "",
+        };
+
+        format!("{:.1} mmol/L{}",
+            &self.sgv_as_mmol(),
+            range_state,
+        )
     }
 
     fn to_delta_str(&self) -> String {
-        let add = if self.delta > 0 {
+        let add = if self.delta > 0.0 {
             '+'
         } else {
             '\0'
@@ -62,25 +87,48 @@ impl NsResults {
     }
 }
 
+fn get_last_change() -> activity::Timestamps {
+    let current = get_ns_data().unwrap().get_range();
+    let request_url = "https://nightscout.ransomti.me/api/v1/entries.json?count=100";
+    let res: Vec<NsResults> = reqwest::blocking::get(request_url)
+                               .unwrap().json().unwrap();
+    let mut last_ts = res[0].to_timestamp();
+    for e in res.iter() {
+        if e.get_range() != current {
+            return last_ts;
+        }
+        last_ts = e.to_timestamp();
+    }
+    get_ns_data().unwrap().to_timestamp()
 
-
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = DiscordIpcClient::new("523786960947380245")?;
+    println!("connecting to ns");
+
+    let mut range = get_ns_data().unwrap().get_range();
+    let mut last_change = get_last_change();
+
+    println!("connecting to discord");
     client.connect()?;
     loop {
         let data = get_ns_data()?;
+        if data.get_range() != range {
+            last_change = data.to_timestamp();
+            range = data.get_range();
+        }
         client.set_activity(activity::Activity::new()
             .state(data.to_delta_str().as_str())
             .details(data.to_status_str().as_str())
-            .timestamps(data.to_timestamp())
+            .timestamps(last_change.clone())
         )?;
-        thread::sleep(time::Duration::from_secs(15));
+        thread::sleep(time::Duration::from_secs(5));
     }
 }
 
 fn get_ns_data() -> Result<NsResults,reqwest::Error>{
-    let request_url = "https://nightscout.ransomti.me/api/v1/entries/sgv?count=1";
+    let request_url = "https://nightscout.ransomti.me/api/v1/entries.json?count=1";
     let res: Vec<NsResults> = reqwest::blocking::get(request_url)
                                .unwrap().json().unwrap();
     Ok(res[0].clone())
